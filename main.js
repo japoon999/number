3d0aaa220d41a7fed5d79f91ad644531b08d8f24
japoon999 <aziz.azizli.95@inbox.ru>
2023-11-01 17:41:26 +0400

    number total programing

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var cemi = 0;
+
+
+for (let index = 0; index < array.length; index++) {
+
+    cemi = cemi + array[index]
+}
+
+console.log("arraydaki ededlerin cemi:" + cemi);
+
+
